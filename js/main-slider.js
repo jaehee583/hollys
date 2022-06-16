73f1f7711497e6b116a3888d7ed15647b71865f7
jaehee583 <67506931+jaehee583@users.noreply.github.com>
2022-06-16 21:09:40 +0900

    Add files via upload

---
+++ js/main-slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        mode: 'fade',
+        speed: 1000,
+        controls: false,
+        auto: true,
+        pause: 2000,
+    });
+});
